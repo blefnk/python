7e8a99924e21f4ebd0839a8a3293ce11a764b152
Nazar Kornienko <blefnk@gmail.com>
2022-06-12 18:53:24 +0200

    Initial upload

---
+++ index.py
@@ -0,0 +1,10 @@
+# index.py
+
+def sum(a, b, c):
+    return (a + b + c)
+
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+c = int(input('Enter 3d number: '))
+
+print(f'Sum of {a} and {b} and {c} is {sum(a, b, c)}')
